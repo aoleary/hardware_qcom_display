LineageOS/android_hardware_qcom_display

display: Add display headers lib
LOCAL_COPY_HEADERS is deprecated. Replace with
headers lib to avoid build warnings.

Change-Id: I5512cec25ad8f93cbdf97ab817e608e8628d45ff

Android.mk
@@ -16,4 +16,17 @@ ifneq ($(filter msm8226 msm8x26 msm8960 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)
 endif
 endif
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := display_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+    $(display_top)/libcopybit \
+    $(display_top)/libexternal \
+    $(display_top)/libgralloc \
+    $(display_top)/libhwcomposer \
+    $(display_top)/liboverlay \
+    $(display_top)/libqdutils \
+    $(display_top)/libqservice \
+    $(display_top)/libvirtual
+include $(BUILD_HEADER_LIBRARY)
+
 endif

aoleary/hardware_qcom_display

Android.mk
@@ -26,4 +26,16 @@ ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
 endif
 endif
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := display_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+    $(display_top)/libcopybit \
+    $(display_top)/libgralloc \
+    $(display_top)/libhdmi \
+    $(display_top)/libhwcomposer \
+    $(display_top)/liboverlay \
+    $(display_top)/libqdutils \
+    $(display_top)/libqservice
+include $(BUILD_HEADER_LIBRARY)
+
 endif